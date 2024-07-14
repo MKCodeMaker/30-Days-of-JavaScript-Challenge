fb3b287811236bb9eb2260153f2f607fb8e12004
Manish Kumar <manishkumar54401@gmail.com>
2024-07-14 22:45:58 +0530

    Add files via upload

---
+++ Day_01_Variables_and_Data_Types.js
@@ -0,0 +1,61 @@
+//Activity 1: Variable Declaration
+
+// Task 1: Declare a variableusing [var], assign it a number, and log the value to the console.
+
+var num = 55
+console.log(num);
+
+// Task 2: Declare a variable using [let], assign it a string, and the value to the console.
+
+let first_Name = "Manish"
+console.log(first_Name);
+
+
+// Activity 2: Constant Declaration
+// Task 3: Declare a variable using [const], assign it a boolean value, and log the value to console.
+
+const isLearningCode = true
+console.log(isLearningCode);
+console.log(typeof isLearningCode);
+
+
+//Activity 3: Data Types 
+// Task 4: Create variables of different data types(number, string, boolean, object, array) and log each variable's type using the [typeof]  operator.
+
+let nameCode = 721
+let yourName = "Manish kumar"
+let isValuable = true
+let detailsOfCoder ={
+    CodeName : "Coder Maker",
+    whichLanguage : "Javascript",
+    whereYouLearn : "Chai aur Code yt Channel",
+}
+let allNameCode = ['nameCode', 'yourName']
+
+console.log(typeof nameCode);
+console.log(typeof yourName);
+console.log(typeof isValuable);
+console.log(typeof detailsOfCoder);
+console.log(typeof allNameCode);
+
+
+// Activity 4: Reassigning Variables
+// Task 5: Declare a variable using[let], assign it an initial value, reassign a new value, and log both values, and log both values to the console.
+
+let challenge_day = 6
+console.log(challenge_day);
+
+challenge_day = 1
+console.log(challenge_day);
+
+
+// Activity 5: Understanding [const]
+// Task 6: Try reassigning a variable declared with [const] and obseve the error.
+
+const rate_the_challenge = 5
+console.log(rate_the_challenge);
+
+rate_the_challenge = 4
+console.log(rate_the_challenge);
+
+
---
+++ Day_02_Operators.js
@@ -0,0 +1,133 @@
+// Activity 1: Arithmetic Operations
+// Task 1: Write a program to add two numbers and log the result to the console.
+
+let num1 = 5
+let num2 = 7
+let sum = num1 + num2 
+
+console.log(sum);
+
+// Task 2: write a program to subtract two numbers and log the result to the console.
+
+let num3 = 55
+let num4 = 34
+let subtract = num3 - num4
+
+console.log(subtract);
+
+// Task 3: Write a program to subtract multiply two numbers and log the result to the console.
+
+let num5 = 6
+let num6 = 7
+let multi = num5*num6
+
+console.log(multi);
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+
+let num7 = 60
+let num8 = 6
+let div = num7/num8
+
+console.log(div);
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+let num9 = 47
+let num10 = 4
+let rem = num9 % num10
+
+console.log(rem);
+
+// Activity 2: Assignment Operators
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let firstNumber = 56
+firstNumber += 5
+
+console.log(firstNumber);
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+let otherNumber = 60
+otherNumber -= 5
+
+console.log(otherNumber);
+
+
+// Activity 3: Comparison Operators
+// Task 8: Write a program to acompare two numbers using > and < and log the result to the console.
+
+const number1 = 40
+const number2 = 20
+const greater = number1 > number2
+const smaller = number1 < number2
+
+console.log(greater);
+console.log(smaller);    
+
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+const a = 5
+const b = 3
+const c = 5
+
+let isGreaterThanOrEqual = a >= b
+let isLessThanOrEqual = a <= b
+let isEqualOrGreater = a >= c
+let isEqualOrLess = a <= c
+
+console.log(isGreaterThanOrEqual);
+console.log(isLessThanOrEqual);
+console.log(isEqualOrGreater);
+console.log(isEqualOrLess);
+
+
+// Task 10: Write a Program to comare two number using == and === and log the result to the console.
+
+const x = 7
+const y = "7"
+let isEqual = (x==y)
+let isStrictEqual = (x===y)
+
+console.log(isEqual );
+console.log(isStrictEqual );
+
+// Acticity 4: Logical Operators
+// Task 11: Write a program that uses the && operator to combine two consditions and log the result to the conosle.
+
+const q = 10
+const w = 10
+const e = 4
+const r = 5
+const t = 5
+
+let andOperator = (q == w && r == t);
+console.log(andOperator);
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+
+let orOperator = (q == w || e == r)
+console.log(orOperator);
+
+// Task 13: Write a program that uses the ! operator to combine two conditions and log the result to the console.
+
+let notEqual = !(r == t)
+console.log (notEqual);
+
+
+// Activity 5: Ternary Operators 
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log result to the console.
+
+let positiveNumber = 8
+let negativeNumber = -5
+let result = ( positiveNumber > 0 )? "positive" : "negative";
+let result2 = ( negativeNumber > 0 )? "positive" : "negative";
+
+
+console.log(result);
+console.log(result2);
+
+
+
+
